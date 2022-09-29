f02a96be41c5626e144f81a1c63dc55ccfe323ad
Xingyu <xingyuye@autox.ai>
2022-09-29 18:50:42 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Layout } from 'antd';
+
+// const { Header, Footer, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hello world!
+    </div>
+  );
+};
+
+export default App;
